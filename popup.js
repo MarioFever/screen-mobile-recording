3e4aef1003f054505c1456b5d1e94a92a8a492c5
MarioFever <mario.palencia@feverup.com>
2025-12-13 15:09:00 +0100

    Initialize Screen Mobile Recording extension structure

---
+++ popup.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const startBtn = document.getElementById('start-btn');
+  
+  startBtn.addEventListener('click', () => {
+    console.log('Start recording clicked');
+    // Logic to start recording will go here
+  });
+});
+
